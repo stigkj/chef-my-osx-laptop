a4c2dc68a92865e79c06dc255e6dde548bf1f083
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2012-09-14 11:52:00 +0200

    Upgrade IntelliJ EAP Leda (122.264 --> 122.327)

--- site-cookbooks/nisgits/recipes/default.rb
+++ site-cookbooks/nisgits/recipes/default.rb
@@ -114,8 +114,8 @@ end
 #  type 'mpkg'
 #end
 
-dmg_package 'Leda-IU-122.264' do
-  source 'http://download.jetbrains.com/idea/ideaIU-122.264.dmg'
+dmg_package 'Leda-IU-122.327' do
+  source 'http://download.jetbrains.com/idea/ideaIU-122.327.dmg'
   destination '/Applications/Development/'
 end
 
