32993a5ec57e29a4a65faca1e3fe875869fd8767
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2012-10-21 16:50:33 +0200

    Need to specify the backup path specifically
    If this is not done, chef-solo will not run if run without sudo
    rights, which seems the best option when working with Homebrew

--- solo.rb
+++ solo.rb
@@ -1,4 +1,5 @@
 file_cache_path "/tmp/chef-solo"
+file_backup_path "/tmp/chef-solo/backup"
 cache_options({ :path => "/tmp/chef-solo/checksums", :skip_expires => true })
 
 root = File.expand_path(File.dirname(__FILE__))
