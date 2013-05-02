e9d908d5a14f39e8223064b64e0f9a559b7ad6ac
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2013-05-02 17:47:25 +0200

    Should install vagrant through brew
    Use the brew-cask plugin to enable installation of vagrant.

--- site-cookbooks/nisgits/recipes/default.rb
+++ site-cookbooks/nisgits/recipes/default.rb
@@ -168,6 +168,7 @@ dmg_package 'VirtualBox' do
   type 'mpkg'
 end
 
+# TODO Use brew cask install vagrant
 dmg_package 'Vagrant' do
   source 'http://files.vagrantup.com/packages/eb590aa3d936ac71cbf9c64cf207f148ddfc000a/Vagrant-1.0.3.dmg'
   type 'pkg'
