f43f96e1819a47677d8eeec574607765ed3323b0
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2013-12-21 16:09:45 +0100

    Upgrade IntelliJ to release version (13)

--- site-cookbooks/nisgits/recipes/default.rb
+++ site-cookbooks/nisgits/recipes/default.rb
@@ -260,9 +260,9 @@ dmg_package 'PrinterDriver_MP760_105102' do
 end
 
 intellij_name = 'IntelliJ IDEA'
-intellij_version = ' 13 EAP'
+intellij_version = ' 13'
 intellij_url_name = 'ideaIU'
-intellij_url_version = '-133.79'
+intellij_url_version = '-13'
 dmg_package "#{intellij_name}#{intellij_version}" do
   source "http://download.jetbrains.com/idea/#{intellij_url_name}#{intellij_url_version}.dmg"
   destination '/Applications/Development/'
